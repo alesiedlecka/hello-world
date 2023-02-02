435a7810db5663dc3bd5d65a16ba5ad5def840ef
Aleksandra Siedlecka <123474761+alesiedlecka@users.noreply.github.com>
2023-02-02 21:51:59 +0100

    add currency

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-import sys 
-print(sys.argv)
-print('Hello World')
+import requests
+url = "http://www.floatrates.com/daily/usd.json"
+resp = requests.get(url=url)  
+data = resp.json()
+from tabulate import tabulate
+currencies = ["eur", "gbp"]
+table = []
+for currency in currencies:
+	currency_data = data[currency]
+	table.append([currency_data["name"],currency_data["rate"]])
+print(tabulate(table))
+	
+
